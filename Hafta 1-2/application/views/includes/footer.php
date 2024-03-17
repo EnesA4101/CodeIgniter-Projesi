3ef9a989241bd6954c71fc35eac64552dc2701a2
Enes Arslan <enesarslanresmi@gmail.com>
2024-03-17 06:05:31 +0300

    Ders 1-2
    Tema parçalandı - veriler tablolara çekilkdi.

---
+++ Hafta 1-2/application/views/includes/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer footer-transparent d-print-none">
+          <div class="container-xl">
+            <div class="row text-center align-items-center flex-row-reverse">
+              <div class="col-lg-auto ms-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                  <li class="list-inline-item"><a href="https://github.com/EnesA4101" target="_blank" class="link-secondary" rel="noopener">Github</a></li>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                  <li class="list-inline-item">
+                    Copyright &copy; 2024
+                    <a href="https://github.com/EnesA4101" class="link-secondary">Enes Arslan</a>.
+                    All rights reserved.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
